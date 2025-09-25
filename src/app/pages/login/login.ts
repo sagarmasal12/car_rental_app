5baf5af95d438adb0788a2f3221975809efff9b3
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-25 09:55:18 +0530

    first commit

---
+++ src/app/pages/login/login.ts
@@ -0,0 +1,27 @@
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  templateUrl: './login.html',
+  styleUrl: './login.css'
+})
+export class Login {
+
+  loginObj:any ={
+    userName:'',
+    password:''
+  }
+
+  router = inject(Router)
+  onLogin(){
+    if(this.loginObj.userName=="admin" && this.loginObj.password=="sagar"){
+      this.router.navigateByUrl("/dashboard")
+    }else{
+      alert("Wrong Credential")
+    }
+  }
+  
+}
