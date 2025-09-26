ccc52806abe661021314e5430d149b214bb33d00
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-26 09:45:31 +0530

    vehicle ts

--- src/app/pages/vehicles/vehicles.ts
+++ src/app/pages/vehicles/vehicles.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { APIResponce, CarModel } from '../../model/car';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-vehicles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './vehicles.html',
-  styleUrl: './vehicles.css'
+  styleUrls: ['./vehicles.css']
 })
 export class Vehicles {
+
+  newcarobj:CarModel;
+  http = inject(HttpClient)
+
+  constructor(){
+    this.newcarobj = new CarModel();
+
+  }
+
+  onSaveCar(){
+    debugger
+    this.http.post<APIResponce>("https://freeapi.miniprojectideas.com/api/CarRentalApp/CreateNewCar",this.newcarobj).subscribe((res:APIResponce)=>{
+      debugger
+      if(res.result){
+        alert("Vehicle Created Successfully")
+      }else{
+        alert(res.message)
+      }
+    })
+  }
+
+
 
 }
