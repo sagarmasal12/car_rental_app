bdc0b583b71e68a0fa6f8dfda04d86328b447d13
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-29 11:17:58 +0530

    booking route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Login } from './pages/login/login';
 import { Layout } from './pages/layout/layout';
 import { Dashboard } from './pages/dashboard/dashboard';
 import { Vehicles } from './pages/vehicles/vehicles';
+import { Booking } from './pages/booking/booking';
 
 export const routes: Routes = [
     {
@@ -26,6 +27,10 @@ export const routes: Routes = [
             {
                 path:"vehicles",
                 component:Vehicles
+            },
+            {
+                path:"booking",
+                component:Booking
             }
         ]
     }
