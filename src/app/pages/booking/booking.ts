6ec0898c5420ed1b341e85e39661dfe1c511b2b2
sagarmasal12 <sagarmasal12@gmail.com>
2025-09-30 12:12:44 +0530

    booking reactive form implementation

--- src/app/pages/booking/booking.ts
+++ src/app/pages/booking/booking.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, inject, Inject, OnInit } from '@angular/core';
+import { CarService } from '../../service/car-service';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './booking.html',
   styleUrl: './booking.css'
 })
-export class Booking {
+export class Booking implements OnInit{
 
+  bookingSrv = inject(CarService);
+
+  carList:any[]=[]
+
+  bookingForm = new FormGroup({
+
+  customerName:new FormControl(""),
+  customerCity:new FormControl(""),
+  mobileNo:new FormControl(""),
+  email:new FormControl(""),
+  bookingId:new FormControl(""),
+  carId:new FormControl(""),
+  bookingDate:new FormControl(""),
+  discount:new FormControl(""),
+  totalBillAmount:new FormControl(""),
+  })
+
+  ngOnInit(): void {
+    this.getCarList();
+
+  }
+
+  getCarList(){
+    this.bookingSrv.getAllCars().subscribe((res:any)=>{
+      this.carList= res.data
+    })
+  }
+
+  getBookings(){
+    this.bookingSrv.getAllBooking().subscribe((res:any)=>{
+      this.carList= res.data
+    })
+  }
+  
 }
